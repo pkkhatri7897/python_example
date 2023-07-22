cdf51f925736205c84927584e031df100e471d43
Parth_Khatri <80441933+pkkhatri7897@users.noreply.github.com>
2023-07-22 12:25:39 +0530

    Create python_ex.py

---
+++ python_ex.py
@@ -0,0 +1,34 @@
+
+class Atm():
+
+    def __init__(self):
+        self.pin = ''
+        self.balance = 0
+
+        self.menu()
+
+    def menu(self):
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
