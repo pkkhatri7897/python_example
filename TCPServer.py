ba426cf0474fbf4dec77ff87eac1519e33634fba
Parth_Khatri <80441933+pkkhatri7897@users.noreply.github.com>
2023-07-25 18:17:12 +0530

    pyGobject integration with python.

--- TCPServer.py
+++ TCPServer.py
@@ -16,21 +16,21 @@ class TCP_server():
     def create_Socket(self):
         try: 
             self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socke.error as error:
+        except socket.error as error:
             print("Socket failed to create!")
             print(f'reason: {error}')
 
     def bind_socket(self, server_addr, port_no):
         try:
             self.server_sock.bind(('127.0.0.1', 1234))
-        except socke.error as error:
+        except socket.error as error:
             print("Socket failed to bind!")
             print(f'reason: {error}')
 
     def listen_socket(self, max_allow_conn):
         try:
             self.server_sock.listen(max_allow_conn)
-        except socke.error as error:
+        except socket.error as error:
             print("Socket failed to listen!")
             print(f'reason: {error}')
         
@@ -38,7 +38,7 @@ class TCP_server():
         try:
             self.client_sock, self.addr = self.server_sock.accept()
             print(f'Client connected from {self.addr}')
-        except socke.error as error:
+        except socket.error as error:
             print("Socket failed to listen!")
             print(f'reason: {error}')
     
